67ef28ff06d7cac78288fa5af2ef4d7c2a4ccb82
blody09 <blakeody9@gmail.com>
2020-03-03 08:39:29 -0500

    first push

---
+++ SW-API/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SW_API.Models;
+
+namespace SW_API.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public async Task<IActionResult> Index()
+
+        {
+
+        }
+
+        public async Task<IActionResult> GetPersonById()
+        {
+            
+        }
+
+
+
+    }
+}
